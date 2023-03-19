375c7c548cb9f321d7084b6483a0da2b46ab5d48
Konstantin Rybak <62746770+K-Rybak@users.noreply.github.com>
2023-03-19 20:05:32 +0600

    added new migration

--- Infrastructure/Entity/Recipe.cs
+++ Infrastructure/Entity/Recipe.cs
@@ -13,6 +13,8 @@ namespace Infrastructure.Entity
         public TimeSpan TimeToCook { get; set; }
         public bool IsDeleted { get; set; }
         public string Method { get; set; }
+        public bool IsVegetirian { get; set; }
+        public bool IsVegan { get; set; }
         public ICollection<Ingredient> Ingredients { get;set; }
     }
 }
